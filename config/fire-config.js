88c0f25df3832911ca143d8ef0c2d195969ed910
Griffin_Jones <jonesg873@gmail.com>
2020-08-22 00:23:37 +0800

    hope

--- config/fire-config.js
+++ config/fire-config.js
@@ -8,10 +8,10 @@ const firebaseConfig = {
     databaseURL: "https://griffins-foodplanner.firebaseio.com",
     projectId: "griffins-foodplanner",
     storageBucket: "griffins-foodplanner.appspot.com",
-    messagingSenderId: "226396313446",
-    appId: "1:226396313446:web:d92b57f97245f3b165daa7"
+    messagingSenderId: 'process.env.messagingSenderId',
+    appId: 'process.env.appId'
   };
-  
+
 
   try {
     firebase.initializeApp(firebaseConfig);
